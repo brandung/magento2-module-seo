73fe5129112f6c86ddd66755530f96bfe333beca
David Verholen <david@verholen.com>
2019-01-24 15:33:55 +0100

    make language config scope configurable

--- HrefLang/HrefLangValueProvider.php
+++ HrefLang/HrefLangValueProvider.php
@@ -13,19 +13,36 @@ class HrefLangValueProvider
      * @var ScopeConfigInterface
      */
     private $scopeConfig;
+    /**
+     * @var string
+     */
+    private $localeConfigScope;
 
-    public function __construct(ScopeConfigInterface $scopeConfig)
-    {
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        string $localeConfigScope
+    ) {
         $this->scopeConfig = $scopeConfig;
+        $this->localeConfigScope = $localeConfigScope;
     }
 
-    public function getValue(Store $store, string $configScope = ScopeInterface::SCOPE_STORE): string
+    public function getValue(Store $store): string
     {
         $localeCode = $this->scopeConfig->getValue(
             \Magento\Directory\Helper\Data::XML_PATH_DEFAULT_LOCALE,
-            $configScope,
-            $store->getId()
+            $this->localeConfigScope,
+            $this->getScopeCode($store)
         );
         return strtolower(str_replace('_', '-', $localeCode));
+    }
+
+    private function getScopeCode(Store $store): string
+    {
+        switch ($this->localeConfigScope) {
+            case ScopeInterface::SCOPE_WEBSITE:
+                return (string)$store->getWebsiteId();
+            default:
+                return (string)$store->getId();
+        }
     }
 }
